ab2843fb2102430fd01d0f01460ffa05ce883ef7
Daniil <jdchips@daniils-air.home>
2020-01-11 02:56:56 +0000

    template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Mime\Email;
 use DateTime;
 use DateInterval;
 
-class BookingController extends AbstractController
+class MainController extends AbstractController
 {
     public function index()
     {
