2efb9d42a1ed3d4d763e115d189527b97622f035
Daniil <jdchips@daniils-air.home>
2020-01-11 12:43:19 +0000

    annotations

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,30 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Entity\Bookings;
-use Symfony\Component\Mime\Email;
-use DateTime;
-use DateInterval;
-use Symfony\Component\Routing\Annotation\Route;
+
+
 
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main_page", name="main_page")
+     * @Route("/mainpage", name="main_page")
      */
-
     public function main_page()
     {
         return $this->render('base.html.twig');
     }
 
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="landing")
      */
-
     public function maintenance()
     {
         return $this->render('maintenance.html.twig');
